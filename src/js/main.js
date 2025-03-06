0a0207a09ff7b5d18f1848181092ace2d3ba4af9
Oleksandr Sirenko <oleksa.sirenko@gmail.com>
2025-03-07 00:14:34 +0200

    feat: add initial JavaScript entry point

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+// File: src/js/main.js
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Portfolio website initialized');
+  
+  // Will add more functionality in future tasks
+});
