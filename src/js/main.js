9c91ea7497fcdd3b073e6e96baf5dcb3f2ace10a
Oleksandr Sirenko <oleksa.sirenko@gmail.com>
2025-03-07 00:54:22 +0200

    refactor: update JS with module imports for Vite

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
 // File: src/js/main.js
+import '../styles/main.scss'; // Import main styles
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Portfolio website initialized');
+  console.log('Portfolio website initialized with Vite');
   
-  // Will add more functionality in future tasks
+  // Initialize components (to be added in future tasks)
+  initializeApp();
 });
+
+function initializeApp() {
+  // This function will be used to initialize all components
+  console.log('App initialization logic will be added here');
+}
